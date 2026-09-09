d45655c55a7a79f484f11d3aa2b9e977ed470253
Ivor Padilla <ivorjpc@gmail.com>
2026-09-09 14:04:31 +0200

    sxr 0.9.0: install portable runtimes

--- Formula/sxr.rb
+++ Formula/sxr.rb
@@ -1,51 +1,34 @@
 class Sxr < Formula
-  include Language::Python::Virtualenv
-
   desc "Session x-ray: read Claude Code and Codex sessions from the terminal"
   homepage "https://github.com/ivorpad/sxr"
-  url "https://github.com/ivorpad/sxr/archive/refs/tags/v0.8.1.tar.gz"
-  sha256 "1a6e4167e52ab77a47838d610f22982b8e98eeea4e8344678dbd480a47acc388"
+  version "0.9.0"
   license "MIT"
 
-  depends_on "python@3.13"
-
-  resource "annotated-doc" do
-    url "https://files.pythonhosted.org/packages/57/ba/046ceea27344560984e26a590f90bc7f4a75b06701f653222458922b558c/annotated_doc-0.0.4.tar.gz"
-    sha256 "fbcda96e87e9c92ad167c2e53839e57503ecfda18804ea28102353485033faa4"
+  on_macos do
+    on_arm do
+      url "https://github.com/ivorpad/sxr/releases/download/v0.9.0/sxr-0.9.0-macos-arm64.tar.gz"
+      sha256 "0059cd7785162f2fefc7112c7b639868a37eca3dd8489c5ffcc515682ca5f2ea"
+    end
+    on_intel do
+      url "https://github.com/ivorpad/sxr/releases/download/v0.9.0/sxr-0.9.0-macos-x86_64.tar.gz"
+      sha256 "d960f174aab2f5fd160b144a3482d8c7528d180f75f1993e1cab25667b51c43d"
+    end
   end
 
-  resource "markdown-it-py" do
-    url "https://files.pythonhosted.org/packages/06/ff/7841249c247aa650a76b9ee4bbaeae59370dc8bfd2f6c01f3630c35eb134/markdown_it_py-4.2.0.tar.gz"
-    sha256 "04a21681d6fbb623de53f6f364d352309d4094dd4194040a10fd51833e418d49"
-  end
-
-  resource "mdurl" do
-    url "https://files.pythonhosted.org/packages/d6/54/cfe61301667036ec958cb99bd3efefba235e65cdeb9c84d24a8293ba1d90/mdurl-0.1.2.tar.gz"
-    sha256 "bb413d29f5eea38f31dd4754dd7377d4465116fb207585f97bf925588687c1ba"
-  end
-
-  resource "pygments" do
-    url "https://files.pythonhosted.org/packages/c3/b2/bc9c9196916376152d655522fdcebac55e66de6603a76a02bca1b6414f6c/pygments-2.20.0.tar.gz"
-    sha256 "6757cd03768053ff99f3039c1a36d6c0aa0b263438fcab17520b30a303a82b5f"
-  end
-
-  resource "rich" do
-    url "https://files.pythonhosted.org/packages/c0/8f/0722ca900cc807c13a6a0c696dacf35430f72e0ec571c4275d2371fca3e9/rich-15.0.0.tar.gz"
-    sha256 "edd07a4824c6b40189fb7ac9bc4c52536e9780fbbfbddf6f1e2502c31b068c36"
-  end
-
-  resource "shellingham" do
-    url "https://files.pythonhosted.org/packages/58/15/8b3609fd3830ef7b27b655beb4b4e9c62313a4e8da8c676e142cc210d58e/shellingham-1.5.4.tar.gz"
-    sha256 "8dbca0739d487e5bd35ab3ca4b36e11c4078f3a234bfce294b0a0291363404de"
-  end
-
-  resource "typer" do
-    url "https://files.pythonhosted.org/packages/37/78/fda3361b56efc27944f24225f6ecd13d96d6fcfe37bd0eb34e2f4c63f9fc/typer-0.27.0.tar.gz"
-    sha256 "629bd12ea5d13a17148125d9a264f949eb171fb3f120f9b04d85873cab054fa5"
+  on_linux do
+    on_arm do
+      url "https://github.com/ivorpad/sxr/releases/download/v0.9.0/sxr-0.9.0-linux-arm64.tar.gz"
+      sha256 "5d10c2dc153946944db715b44b00173ce13d972cb1053949be35938926a3cbdf"
+    end
+    on_intel do
+      url "https://github.com/ivorpad/sxr/releases/download/v0.9.0/sxr-0.9.0-linux-x86_64.tar.gz"
+      sha256 "b7902b22f444260f2b6f14ed8527bd94a5b914f2a15f82c13f4b092f801b3767"
+    end
   end
 
   def install
-    virtualenv_install_with_resources
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"sxr"
   end
 
   test do
